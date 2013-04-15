1ea13edf605788354faff236c52e83b1c239d7a9
Flavio W. Brasil <fwbrasil@gmail.com>
2013-04-15 02:43:11 -0300

    inference error protection

--- src/main/scala/net/fwbrasil/sReflection/SConstructor.scala
+++ src/main/scala/net/fwbrasil/sReflection/SConstructor.scala
@@ -6,19 +6,19 @@ import java.lang.reflect.{ Constructor => JConstructor, Method => JMethod, Array
 import java.lang.reflect.InvocationTargetException
 import java.lang.{ Class => JClass }
 
-class SConstructor[C](val clazz: Class[C], symbol: MethodSymbol) extends SBehavior[C](symbol, "new") {
-	type JType = JConstructor[C]
-	def jBehavior =
-		try
-			clazz.getConstructor(parametersTypes: _*)
-		catch {
-			case e: NoSuchMethodException =>
-				if (clazz.getDeclaringClass != null)
-					clazz.getConstructor(List(clazz.getDeclaringClass).++(parametersTypes): _*)
-				else throw e
-		}
-	val returnType =
-		clazz
-	def invoke(args: Object*) =
-		jBehavior.newInstance(args: _*).asInstanceOf[Object]
+class SConstructor[C](val clazz: Class[C], symbol: MethodSymbol) extends SBehavior[C](symbol, "novo") {
+    type JType = JConstructor[C]
+    def jBehavior =
+        try
+            clazz.getConstructor(parametersTypes: _*)
+        catch {
+            case e: NoSuchMethodException =>
+                if (clazz.getDeclaringClass != null)
+                    clazz.getConstructor(List(clazz.getDeclaringClass).++(parametersTypes): _*)
+                else throw e
+        }
+    val returnType =
+        clazz
+    def invoke(args: Object*) =
+        jBehavior.newInstance(args: _*).asInstanceOf[Object]
 }
