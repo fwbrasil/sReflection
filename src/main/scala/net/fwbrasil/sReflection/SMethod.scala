573e3abe00c2c2584f5a2993741505cafd702080
Flavio W. Brasil <fwbrasil@gmail.com>
2013-04-14 22:21:47 -0300

    inference error protection

--- src/main/scala/net/fwbrasil/sReflection/SMethod.scala
+++ src/main/scala/net/fwbrasil/sReflection/SMethod.scala
@@ -8,25 +8,32 @@ import java.lang.{ Class => JClass }
 import SReflection._
 
 class SMethod[C](val clazz: Class[C], val symbol: MethodSymbol) extends SBehavior[C](symbol, symbol.name.trim) {
-	type JType = JMethod
-	def jBehavior =
-		try {
-			clazz.getDeclaredMethod(name, parametersTypes: _*)
-		} catch {
-			case e: NoSuchMethodException =>
-				clazz.getDeclaredMethods.find(
-					c => c.getName == name && c.getParameterTypes.size == parametersTypes.size).getOrElse(throw e)
-		}
-	val returnType =
-		jBehavior.getReturnType
-	val fieldGenericParameters =
-		if (symbol.isAccessor && symbol.infoType.isInstanceOf[NullaryMethodType])
-			findGenericParameters(
-				symbol.infoType.asInstanceOf[NullaryMethodType].resultType)
-		else List()
-	def invoke(args: Object*) =
-		if (args.isEmpty)
-			throw new IllegalArgumentException("First parameter must be the receiver")
-		else
-			jBehavior.invoke(args(0), args.tail: _*)
+    type JType = JMethod
+    def jBehavior =
+        try {
+            clazz.getDeclaredMethod(name, parametersTypes: _*)
+        } catch {
+            case e: NoSuchMethodException =>
+                try {
+                    clazz.getDeclaredMethods.find(
+                        c => c.getName == name && c.getParameterTypes.size == parametersTypes.size).getOrElse(throw e)
+                } catch {
+                    case e => null
+                }
+        }
+    val returnType =
+        if (jBehavior == null)
+            classOf[Object].asInstanceOf[Class[_]]
+        else
+            jBehavior.getReturnType
+    val fieldGenericParameters =
+        if (symbol.isAccessor && symbol.infoType.isInstanceOf[NullaryMethodType])
+            findGenericParameters(
+                symbol.infoType.asInstanceOf[NullaryMethodType].resultType)
+        else List()
+    def invoke(args: Object*) =
+        if (args.isEmpty)
+            throw new IllegalArgumentException("First parameter must be the receiver")
+        else
+            jBehavior.invoke(args(0), args.tail: _*)
 }
