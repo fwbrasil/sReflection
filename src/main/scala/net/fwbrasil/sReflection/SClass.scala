3f6ee83e04df9811c41c6a29c20ae7df976cc2ef
Flavio W. Brasil <fwbrasil@gmail.com>
2013-04-19 00:54:25 -0300

    no message

--- src/main/scala/net/fwbrasil/sReflection/SClass.scala
+++ src/main/scala/net/fwbrasil/sReflection/SClass.scala
@@ -9,73 +9,74 @@ import ScalaSigReader._
 import SReflection._
 
 class SClass[C](val clazz: Class[C], symbol: ClassSymbol) extends SymbolVisibility(symbol) {
-	private val methodSymbols =
-		symbol.children.toList collect {
-			case m: MethodSymbol => m
-		}
-	private def collect[T](f: (SClass[C]) => List[T])(f1: => List[T]) = {
-			def isScalaClass(clazz: JClass[_]) =
-				clazz != null && findScalaSig(clazz).isDefined
-		var res = List[T]()
-		res ++= f1
-		if (isScalaClass(clazz.getSuperclass))
-			res ++= f(SClass[C](clazz.getSuperclass.asInstanceOf[JClass[C]]))
-		for (int <- clazz.getInterfaces; if (isScalaClass(int)))
-			res ++= f(SClass[C](int.asInstanceOf[JClass[C]]))
-		res.distinct
-	}
-	val sConstructors: List[SConstructor[C]] =
-		collect(_.sConstructors) {
-			if (symbol.isTrait)
-				List()
-			else
-				methodSymbols.filter(isConstructor).map(new SConstructor(clazz, _))
-		}
-	private val fields =
-		methodSymbols.filter(m => !isInit(m) && m.isLocal)
-	private val accessors: List[MethodSymbol] =
-		methodSymbols.filter(m => !isInit(m) && m.isAccessor)
-	private val accessorsNames =
-		accessors.map(_.name)
-	private def isMethod(m: Symbol) = {
-		m.isMethod && !isInit(m) && !m.isLocal && !m.isPrivate && !accessorsNames.contains(m.name.trim) && !m.name.contains("$default$")
-	}
-	val sMethods: List[SMethod[C]] =
-		collect(_.sMethods) {
-			methodSymbols.filter(isMethod).map(new SMethod(clazz, _))
-		}
-	val companionOption = getCompanionObject(clazz).asInstanceOf[Option[C]]
-	val sCompanionMethods: List[SMethod[C]] = {
-		if (companionOption.isDefined) {
-			val scalaSig = findScalaSig(clazz).get
-			val companionSymbolOption = (scalaSig.topLevelObjects collect { case o: ObjectSymbol if o.name == clazz.getSimpleName => o }).headOption
-			if (companionSymbolOption.isDefined)
-				companionSymbolOption.get.infoType.asInstanceOf[TypeRefType].symbol.children.toList.filter(isMethod) collect {
-					case symbol: MethodSymbol =>
-						new SMethod[C](companionOption.get.getClass.asInstanceOf[Class[C]], symbol)
-				}
-			else
-				List()
-		} else
-			List()
-	}
-	val sFields: List[SField[C]] =
-		collect(_.sFields) {
-			for (getter <- accessors; if (!getter.isPrivate && !getter.name.endsWith("_$eq")))
-				yield new SField(clazz, new SMethod(clazz, getter), methodSymbols.find(_.name == getter.name + "_$eq").map(new SMethod(clazz, _)))
-		}
-	def sBehaviors =
-		sConstructors ++ sMethods
+    private val methodSymbols =
+        symbol.children.toList collect {
+            case m: MethodSymbol => m
+        }
+    private def collect[T](f: (SClass[C]) => List[T])(f1: => List[T]) = {
+        def isScalaClass(clazz: JClass[_]) =
+            clazz != null && findScalaSig(clazz).isDefined
+        var res = List[T]()
+        res ++= f1
+        if (isScalaClass(clazz.getSuperclass))
+            res ++= f(SClass[C](clazz.getSuperclass.asInstanceOf[JClass[C]]))
+        for (int <- clazz.getInterfaces; if (isScalaClass(int)))
+            res ++= f(SClass[C](int.asInstanceOf[JClass[C]]))
+        res.distinct
+    }
+    val sConstructors: List[SConstructor[C]] =
+        collect(_.sConstructors) {
+            if (symbol.isTrait)
+                List()
+            else
+                methodSymbols.filter(isConstructor).map(new SConstructor(clazz, _))
+        }
+    private val fields =
+        methodSymbols.filter(m => !isInit(m) && m.isLocal)
+    private val accessors: List[MethodSymbol] =
+        methodSymbols.filter(m => !isInit(m) && m.isAccessor)
+    private val accessorsNames =
+        accessors.map(_.name)
+    private def isMethod(m: Symbol) = {
+        m.isMethod && !isInit(m) && !m.isLocal && !m.isPrivate && !accessorsNames.contains(m.name.trim) && !m.name.contains("$default$")
+    }
+    val sMethods: List[SMethod[C]] =
+        collect(_.sMethods) {
+            methodSymbols.filter(isMethod).map(new SMethod(clazz, _))
+        }
+    val companionOption = getCompanionObject(clazz).asInstanceOf[Option[C]]
+    val sCompanionMethods: List[SMethod[C]] = {
+        if (companionOption.isDefined) {
+            val scalaSig = findScalaSig(clazz).get
+            val companionSymbolOption = (scalaSig.topLevelObjects collect { case o: ObjectSymbol if o.name == clazz.getSimpleName => o }).headOption
+            if (companionSymbolOption.isDefined)
+                companionSymbolOption.get.infoType.asInstanceOf[TypeRefType].symbol.children.toList.filter(isMethod) collect {
+                    case symbol: MethodSymbol =>
+                        new SMethod[C](companionOption.get.getClass.asInstanceOf[Class[C]], symbol)
+                }
+            else
+                List()
+        } else
+            List()
+    }
+    val sFields: List[SField[C]] =
+        collect(_.sFields) {
+            for (getter <- accessors; if (!getter.isPrivate && !getter.name.endsWith("_$eq")))
+                yield new SField(clazz, new SMethod(clazz, getter), methodSymbols.find(_.name == getter.name + "_$eq").map(new SMethod(clazz, _)))
+        }
+    def sBehaviors =
+        sConstructors ++ sMethods
 
-	def isInit(methodSymbol: Symbol) =
-		methodSymbol.name.trim == "<init>" || methodSymbol.name.trim == "$init$"
-	def isConstructor(methodSymbol: Symbol) =
-		methodSymbol.name.trim == "<init>"
-	private def erasureOf[T: Manifest] =
-		manifest[T].runtimeClass.asInstanceOf[Class[T]]
+    def isInit(methodSymbol: Symbol) =
+        methodSymbol.name.trim == "<init>" || methodSymbol.name.trim == "$init$"
+    def isConstructor(methodSymbol: Symbol) =
+        methodSymbol.name.trim == "<init>"
+    private def erasureOf[T: Manifest] =
+        manifest[T].runtimeClass.asInstanceOf[Class[T]]
 }
 
 object SClass {
-	val cache = new HashMap[Class[_], SClass[_]]() with SynchronizedMap[Class[_], SClass[_]]
-	def apply[C](clazz: Class[C]) = cache.getOrElseUpdate(clazz, new SClass(clazz, findClass(clazz))).asInstanceOf[SClass[C]]
+    val cache = new HashMap[Class[_], Option[SClass[_]]]() with SynchronizedMap[Class[_], Option[SClass[_]]]
+    def find[C](clazz: Class[C]) = cache.getOrElseUpdate(clazz, findClass(clazz).map(new SClass(clazz, _))).asInstanceOf[Option[SClass[C]]]
+    def apply[C](clazz: Class[C]) = find[C](clazz).getOrElse(throw new IllegalStateException("Cant find the scala signature."))
 }
--- src/main/scala/net/fwbrasil/sReflection/ScalaSigReader.scala
+++ src/main/scala/net/fwbrasil/sReflection/ScalaSigReader.scala
@@ -8,33 +8,31 @@ import scala.tools.scalap.scalax.rules.scalasig.ScalaSigParser
 import scala.tools.scalap.scalax.rules.scalasig.TypeRefType
 
 object ScalaSigReader {
-	def fail(msg: String) = throw new IllegalStateException(msg)
+    def fail(msg: String) = throw new IllegalStateException(msg)
 
-	@transient
-	private var cache: HashMap[Class[_], Option[ScalaSig]] = null
+    @transient
+    private var cache: HashMap[Class[_], Option[ScalaSig]] = null
 
-	def findScalaSig(clazz: Class[_]): Option[ScalaSig] =
-		if (clazz == null)
-			None
-		else {
-			if (cache == null)
-				cache = new HashMap[Class[_], Option[ScalaSig]]() with SynchronizedMap[Class[_], Option[ScalaSig]]
-			cache.getOrElseUpdate(clazz, ScalaSigParser.parse(clazz).orElse(findScalaSig(clazz.getDeclaringClass)))
-		}
+    def findScalaSig(clazz: Class[_]): Option[ScalaSig] =
+        if (clazz == null)
+            None
+        else {
+            if (cache == null)
+                cache = new HashMap[Class[_], Option[ScalaSig]]() with SynchronizedMap[Class[_], Option[ScalaSig]]
+            cache.getOrElseUpdate(clazz, ScalaSigParser.parse(clazz).orElse(findScalaSig(clazz.getDeclaringClass)))
+        }
 
-	def findClass(clazz: Class[_]): ClassSymbol = {
-		val sig = findScalaSig(clazz).getOrElse(fail("Can't find ScalaSig for " + clazz))
-		findClass(sig, clazz).getOrElse(fail("Can't find " + clazz + " from parsed ScalaSig"))
-	}
+    def findClass(clazz: Class[_]): Option[ClassSymbol] =
+        findScalaSig(clazz).flatMap(findClass(_, clazz))
 
-	private def findClass(sig: ScalaSig, clazz: Class[_]): Option[ClassSymbol] = {
-		sig.symbols.collect { case c: ClassSymbol if !c.isModule => c }.find(_.name == clazz.getSimpleName).orElse {
-			sig.topLevelClasses.find(_.symbolInfo.name == clazz.getSimpleName).orElse {
-				sig.topLevelObjects.map { obj =>
-					val t = obj.infoType.asInstanceOf[TypeRefType]
-					t.symbol.children collect { case c: ClassSymbol => c } find (_.symbolInfo.name == clazz.getSimpleName)
-				}.head
-			}
-		}
-	}
+    private def findClass(sig: ScalaSig, clazz: Class[_]): Option[ClassSymbol] = {
+        sig.symbols.collect { case c: ClassSymbol if !c.isModule => c }.find(_.name == clazz.getSimpleName).orElse {
+            sig.topLevelClasses.find(_.symbolInfo.name == clazz.getSimpleName).orElse {
+                sig.topLevelObjects.map { obj =>
+                    val t = obj.infoType.asInstanceOf[TypeRefType]
+                    t.symbol.children collect { case c: ClassSymbol => c } find (_.symbolInfo.name == clazz.getSimpleName)
+                }.head
+            }
+        }
+    }
 }
